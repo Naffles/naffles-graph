6cbff66c41c1f28c078cfe97274f65d6dc76afe6
root <r00tail@protonmail.com>
2023-10-17 13:55:01 +0200

    Resolved issue as well as simplified mapping

--- foundersstakinglegacy/src/founders-key-staking.ts
+++ foundersstakinglegacy/src/founders-key-staking.ts
@@ -6,15 +6,13 @@ import { Stake } from "../generated/schema";
 import { Bytes, store } from '@graphprotocol/graph-ts';
 
 export function handleUserStaked(event: UserStakedEvent): void {
-  let userAddress = event.params.userAddress;
-  let stakeId = userAddress.toHex().concat("-").concat(event.params.nftId.toString());
-  let stake = new Stake(Bytes.fromHexString(stakeId));
+  let stake = new Stake(event.params.nftId.toString());
   stake.nftId = event.params.nftId;
   stake.stakingPeriod = event.params.stakingPeriod;
+  stake.userAddress = event.params.userAddress;
   stake.save();
 }
 
 export function handleUserUnstaked(event: UserUnstakedEvent): void {
-  let stakeId = event.params.userAddress.toHex().concat("-").concat(event.params.nftId.toString());
-  store.remove("Stake", stakeId);
+  store.remove("Stake", event.params.nftId.toString());
 }
