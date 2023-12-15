eeaed1356359c6cefdc5f26ffd23209772ed8d65
c0dejax <jaxdoubleu@protonmail.com>
2023-12-15 07:24:25 +0100

    distinguish between legacy and new

--- foundersstakinglegacy/src/founders-key-staking.ts
+++ foundersstakinglegacy/src/founders-key-staking.ts
@@ -3,16 +3,50 @@ import {
   UserUnstaked as UserUnstakedEvent
 } from "../generated/FoundersKeyStaking/FoundersKeyStaking";
 import { Stake, StakeHistory, StakeSession} from "../generated/schema";
-import { Bytes, bigInt, store } from '@graphprotocol/graph-ts';
-import { log } from '@graphprotocol/graph-ts'
+import { store } from '@graphprotocol/graph-ts';
 
-export function handleUserStaked(event: UserStakedEvent): void {
+export function handleUserStakedLegacy(event: UserStakedEvent): void {
   let stake = new Stake(event.params.nftId.toString());
   stake.nftId = event.params.nftId;
   stake.stakingPeriod = event.params.stakingPeriod;
   stake.userAddress = event.params.userAddress;
   stake.save();
+  
+  handleUserStaked(event); 
+}
 
+export function handleUserUnstakedLegacy(event: UserUnstakedEvent): void {
+  store.remove("Stake", event.params.nftId.toString());
+  _handleUnstaked(event);
+}
+
+export function handleUserStaked(event: UserStakedEvent): void {
+  _handleStaked(event);
+}
+
+export function handleUserUnstaked(event: UserUnstakedEvent): void {
+  _handleUnstaked(event); 
+}
+
+function _handleUnstaked(event: UserUnstakedEvent): void {
+  let id = event.params.nftId.toString() + "-" + event.params.userAddress.toHex();
+  let stakeHistory = StakeHistory.load(id);
+  if (stakeHistory == null) {
+    return;
+  }
+
+  const lastId = stakeHistory.stakeSessionIds.at(stakeHistory.stakeSessionIds.length - 1)
+  const stakeSession = StakeSession.load(lastId);
+  if (stakeSession== null) {
+    return;
+  }
+  if (stakeSession.unstakedAt === null) {
+    stakeSession.unstakedAt = event.block.timestamp;
+    stakeSession.save();
+  }
+}
+
+function _handleStaked(event: UserStakedEvent): void {
   let stakeHistoryId = event.params.nftId.toString() + "-" + event.params.userAddress.toHex();
   
   // load the StakeHistory entity for this NFT and user
@@ -38,24 +72,5 @@ export function handleUserStaked(event: UserStakedEvent): void {
   stakeSessions.push(stakeSessionId);
   stakeHistory.stakeSessionIds = stakeSessions;
   stakeHistory.save();
-}
 
-export function handleUserUnstaked(event: UserUnstakedEvent): void {
-  store.remove("Stake", event.params.nftId.toString());
-
-  let id = event.params.nftId.toString() + "-" + event.params.userAddress.toHex();
-  let stakeHistory = StakeHistory.load(id);
-  if (stakeHistory == null) {
-    return;
-  }
-
-  const lastId = stakeHistory.stakeSessionIds.at(stakeHistory.stakeSessionIds.length - 1)
-  const stakeSession = StakeSession.load(lastId);
-  if (stakeSession== null) {
-    return;
-  }
-  if (stakeSession.unstakedAt === null) {
-    stakeSession.unstakedAt = event.block.timestamp;
-    stakeSession.save();
-  } 
 }
